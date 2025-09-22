8a7c52dc73e0e66698bb4a5ba83167eebc558430
Danielambrose-web <danielessientech121@gmail.com>
2025-09-22 08:42:15 +0100

    continue with loops

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 let firstCard = 6
 let secondCard = 1
 let thirdCard = 7
-
 let cards = [firstCard, secondCard]
-
 let sum = firstCard + secondCard
+
 let hashBlackJack = false
 let isAlive = true
 
 let message = ""
-
 messageEl = document.getElementById('message-el')
-//sumEl=document.getElementById('sum-el')
 sumEl = document.querySelector('#sum-el')
 cardsEl = document.querySelector('#card-el')
 sumel = document.querySelector('#sum-el')
@@ -22,10 +19,14 @@ function startGame() {
 
 
 function game__init__() {
+    cardsEl.textContent = 'cards:'
+
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = 'sum:' + ' ' + sum
-
-    cardsEl.textContent = 'cards:' + '' + cards[0] + " " + cards[1]
-
     if (sum <= 20) {
         message = 'do u want to draw a new card'
     }
@@ -37,53 +38,14 @@ function game__init__() {
         message = ' u are out'
         isAlive = false
     }
-
     messageEl.textContent = message
 }
 
 function newCard() {
-    sum = sum + thirdCard
-    game__init__()
+    sum +=  thirdCard
     cards.push(thirdCard)
-
+    game__init__()
+ 
     //window.alert(cards +' and'+cards.length)
-   
+
 }
-
-
-function reset() {
-    sumEl.textContent = "sum:" + '' + 0
-    cardsEl.textContent = 'cards:' + '' + 0
-    sumel.textContent = 'sum:' + ' ' + 0
-}
-
-/*
-
-let x = 20
-
-for( count = 1; count < x; count += 1){
-    console.log(count)
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
