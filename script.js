dcbc77bc0ab804b93eb6ac572f9851eea13795f3
Danielambrose-web <danielessientech121@gmail.com>
2025-09-22 08:42:30 +0100

    just like a note book

---
+++ script.js
@@ -0,0 +1,39 @@
+/*let count = 0;  // Initialize count to 0 for array indexing
+let cards = ['firstCard', 'secondCard', 'essens', 'hesusidfm', 'egbc'];
+
+// Remove last element - pop() doesn't need an argument
+cards.pop();
+console.log(cards.length);
+cards.push('nwe', 'dF', 'dfv');
+
+// Correct for loop syntax to iterate through array
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i]);
+}
+
+
+let player1Time = 108;
+let player2Time = 107;
+
+function getFastestRaceTime(){
+    if (player1Time < player2Time) {
+        return player1Time;}
+    else if (player2Time < player1Time) {
+        return player2Time;}
+    else {
+        return player1Time;}     
+    }
+
+    let fstestRaceTime = getFastestRaceTime();
+    console.log(fstestRaceTime);*/
+
+   /* racer1Time = 102;
+    racer2Time = 107;
+    racer3Time = 101;   
+
+    function racersTotalTime(){
+        return racer1Time + racer2Time + racer3Time;
+    }
+
+    let totalTime = racersTotalTime();
+    console.log(totalTime);*/
